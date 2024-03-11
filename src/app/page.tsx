14ded0fd9535cef7391fb89d083c60d4c98537f6
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2024-03-11 22:02:29 +0530

    aaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,12 @@ export default function Home() {
   return (
     <main className="dark w-screen min-h-screen overflow-x-hidden bg-black flex flex-col items-center justify-center gap-10">
       <BackgroundBeams/>
-      <h1 className="text-3xl md:text-4xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
+      <h1 className="text-3xl md:text-4xl bg-clip-text  text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold relative z-10">
         Contact Page</h1>
-      <div className="w-3/5 h-3/5 bg-zinc-800 p-10">
-        <div className="w-1/3 flex flex-col gap-2">
+        <div className="w-1/3 flex flex-col gap-2 relative z-10">
         <label htmlFor="email" className="font-bold text-white relative">Email<span className="text-red-800 absolute -top-2">*</span></label>
-          <Input type="email" id="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          <Input className="relative z-10 text-white" type="email" id="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
         </div>
-      </div>
       
     </main>
   );
